1ab8d7d294ad741c6644c0d2259c9fe809610589
cooklee <sbbogus@gmail.com>
2023-10-16 09:50:12 +0200

    index view + task,timespend models

---
+++ tracker/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+    parent = models.ForeignKey('Task', null=True, on_delete=models.CASCADE)
+
+class TimeSpent(models.Model):
+    amount = models.IntegerField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.CharField(max_length=512)
+
+
+
